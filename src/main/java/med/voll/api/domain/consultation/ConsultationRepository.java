cf9b705121d9792aff01e4cb9777e038cfda2c1a
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-06-26 23:08:03 -0600

    Last update for today

--- src/main/java/med/voll/api/domain/consultation/ConsultationRepository.java
+++ src/main/java/med/voll/api/domain/consultation/ConsultationRepository.java
@@ -1,6 +1,5 @@
 package med.voll.api.domain.consultation;
 
-import med.voll.api.domain.pacient.Pacient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface ConsultationRepository extends  JpaRepository<Consultation, Long> {
-    Boolean existsByPacientAndDateTimeBetween(Long pacient, LocalDateTime firstDateTime, LocalDateTime lastDateTime);
+    Boolean existsByPacientIdAndDateTimeBetween(Long pacientId, LocalDateTime firstDateTime, LocalDateTime lastDateTime);
 
     Boolean existByMedicIdAndDateTime(Long medicId, LocalDateTime date);
 }
--- src/main/java/med/voll/api/domain/consultation/validations/PacientsWithoutConsultation.java
+++ src/main/java/med/voll/api/domain/consultation/validations/PacientsWithoutConsultation.java
@@ -3,7 +3,6 @@ package med.voll.api.domain.consultation.validations;
 import jakarta.validation.ValidationException;
 import med.voll.api.domain.consultation.ConsultationRepository;
 import med.voll.api.domain.consultation.DataScheduleConsultation;
-import med.voll.api.infra.errors.IntegrityValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,9 +16,9 @@ public class PacientsWithoutConsultation implements ConsultationValidator{
         var firstDateTime = data.dateTime().withHour(7);
         var lastDateTime = data.dateTime().withHour(18);
 
-        var pacientConsultation = repository.existsByPacientAndDateTimeBetween(data.idPacient(), firstDateTime, lastDateTime);
+        var pacientWithConsultation = repository.existsByPacientIdAndDateTimeBetween(data.idPacient(), firstDateTime, lastDateTime);
 
-        if (pacientConsultation) {
+        if (pacientWithConsultation) {
             throw new ValidationException("The pacient has an appointment in this time");
         }
     }
